b7ce2ab15ca538be1ea8084e1bb978c003b992af
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2022-11-17 12:52:25 +0300

    added DALs objects (authAPI, tasksAPI)

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-type TodolistType= {
+type TodolistType = {
     id: string
     addedDate: string
     order: number
@@ -16,19 +16,66 @@ const instance = axios.create({
     }
 });
 
+export const authAPI = {
+    authMe() {
+        return instance.get(`auth/me`);
+    },
+    logIn(email: string, password: string, rememberMe: boolean, captcha: boolean) {
+        let body = {email, password, rememberMe, captcha};
+        return instance.post(`/auth/login`, body);
+    },
+};
+
 export const todolistAPI = {
     getTodoLists() {
-            return instance.get<Array<TodolistType>>(`todo-lists`)
-                .then(response => response.data);
-        },
+        return instance.get<Array<TodolistType>>(`todo-lists`)
+            .then(response => response.data);
+    },
     createTodolist(title: string) {
         return instance.post(`todo-lists`, {title});
     },
     deleteTodolist(todolistId: string) {
-        return instance.delete(`/todo-lists/${todolistId}`);
+        return instance.delete(`todo-lists/${todolistId}`);
     },
     updateTodolistTitle(todolistId: string, title: string) {
-        return instance.put(`/todo-lists/${todolistId}`, {title});
-    }
-
+        return instance.put(`todo-lists/${todolistId}`, {title});
+    },
+    reorderTodoList(todolistId: string, putAfterItemId: string) {
+        let body = {putAfterItemId};
+        return instance.put(`todo-lists/${todolistId}/reorder`, body);
+    },
 };
+
+export const tasksAPI = {
+    getTasks(todolistId: string) {
+        return instance.get(`todo-lists/${todolistId}/tasks`);
+    },
+    createTask(todolistId: string, title: string) {
+        let body = {title};
+        return instance.post(`todo-lists/${todolistId}/tasks`, body);
+    },
+    updateTask(
+        todolistId: string,
+        taskId: string,
+        title: string,
+        description: string,
+        completed: boolean,
+        status: number,
+        priority: number,
+        startDate: string,
+        deadline: string,
+    ) {
+        let body = {title, description, completed, status, priority, startDate, deadline};
+        return instance.put(`todo-lists/${todolistId}/tasks/${taskId}`, body);
+    },
+    deleteTask(todolistId: string, taskId: string) {
+        return instance.delete(`todo-lists/${todolistId}/tasks/${taskId}`);
+    },
+    reorderTask(todolistId: string, taskId: string, putAfterItemId: string) {
+        let body = {putAfterItemId};
+        return instance.put(`todo-lists/${todolistId}/tasks/${taskId}/reorder`, body);
+    },
+};
+
+
+
